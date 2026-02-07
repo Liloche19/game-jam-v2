35a20bf138cd9ef57e20392c5de05e04522a33b0
Liloche19 <lilian.locher@epitech.eu>
2026-02-07 22:40:19 +0100

    feat(ressources): tamarin bicolore added

--- Rooms/Room2/Scripts/InteractableButton.cs
+++ Rooms/Room2/Scripts/InteractableButton.cs
@@ -4,15 +4,17 @@ using System;
 public partial class InteractableButton : StaticBody3D, IInteractable
 {
 	[Export] private CollisionShape3D _chairCollision;
+	[Export] private CollisionShape3D _chairCollision2;
 	[Export] private CollisionShape3D _floorCollision;
 
 	public void Interact()
 	{
-		if (_chairCollision != null && _floorCollision != null)
+		if (_chairCollision != null && _chairCollision2 != null && _floorCollision != null)
 		{
 			// IMPORTANT : On utilise SetDeferred pour éviter les erreurs de physique
 			// On passe la propriété "disabled" à true
 			_chairCollision.SetDeferred(CollisionShape3D.PropertyName.Disabled, true);
+			_chairCollision2.SetDeferred(CollisionShape3D.PropertyName.Disabled, true);
 			_floorCollision.SetDeferred(CollisionShape3D.PropertyName.Disabled, true);
 
 			GD.Print("Collision désactivée ! Le mur est maintenant un bug.");
